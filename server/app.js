889f7853cfa273ac970152b82880c7081cacb42c
Gokberk Otlu <gokberkotlu@gmail.com>
2021-12-12 14:59:06 +0300

    :recycle: refactor: get env port variable inside constant

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ const app = express();
 
 app.use(express.json());
 
+const PORT = process.env.PORT || 4000;
+
 mongoose.connect(process.env.MONGODBURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then((result) => app.listen(process.env.PORT))
+.then((result) => app.listen(PORT))
 .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
