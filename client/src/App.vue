0f0a0b8926977ca2debe9d0a2df898ba2c8934e6
Gokberk Otlu <gokberkotlu@gmail.com>
2021-12-16 04:19:12 +0300

    :sparkles: feat: breadcrumb

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
     <Header />
+    <BreadCrumb :items="breadCrumbItems" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from './components/commons/Header.vue'
+import BreadCrumb from './components/commons/BreadCrumb.vue'
 import './styles/main.css'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    BreadCrumb
+  },
+  data() {
+    return {
+      breadCrumbItems: this.$route.matched.map(match => match.name)
+    }
+  },
+  watch: {
+    $route() {
+      this.breadCrumbItems = this.$route.matched.map(match => match.name);
+    }
   }
 }
 </script>
